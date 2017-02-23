85ca16dc09ec325e86810f333684d15454d1f8f1
Shylaja <shylu2249@gmail.com>
2017-02-23 17:54:53 -0500

    Initial commit

---
+++ src/main/java/com/sidgs/com/sidgs/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.sidgs.com.sidgs.service;
+
+/**
+ * Created by Shylaja on 2/23/2017.
+ */
+public interface MemberService {
+    public void addMember (Memeber member);
+
+}
---
+++ src/main/java/com/sidgs/model/Member.java
@@ -0,0 +1,7 @@
+package com.sidgs.model;
+
+/**
+ * Created by Shylaja on 2/23/2017.
+ */
+public class Member {
+}
